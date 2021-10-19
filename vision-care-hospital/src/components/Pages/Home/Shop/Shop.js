ae470af0981a1c8fe5435f02ca24caf54c13e7f4
iqbalcb88 <iqbal.cb88@gmail.com>
2021-10-19 22:19:58 +0600

    update shop page

--- vision-care-hospital/src/components/Pages/Home/Shop/Shop.js
+++ vision-care-hospital/src/components/Pages/Home/Shop/Shop.js
@@ -1,10 +1,63 @@
 import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import oneShopImg from '../../../../images/1.png';
+import twoShopImg from '../../../../images/2.png';
+import threeShopImg from '../../../../images/3.png';
 
 const Shop = () => {
   return (
     <div>
-      <h1 className='text-center'>Shop</h1>
-      <p className='text-center'>Choose your Glasses and Lenses</p>
+      <div className='services pb-5 '>
+        <h1 className='text-center text-dark-50 py-3 mb-3 fs-1 '>SHOP</h1>
+        <p className='text-center'>Choose your Glasses and Lenses</p>
+        <Container>
+          <Row xs={1} xm={2} md={2} lg={3} className='g-4'>
+            <Col>
+              <Card className='card-hover'>
+                <Card.Img variant='top' src={oneShopImg} />
+                <Card.Header>Anti Glare Glass</Card.Header>
+                <Card.Body>
+                  <Card.Text>Price: 100TK</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Link to=''>
+                    <button className='btn btn-outline-dark'>BUY NOW</button>
+                  </Link>
+                </Card.Footer>
+              </Card>
+            </Col>
+            <Col>
+              <Card className='card-hover'>
+                <Card.Img variant='top' src={twoShopImg} />
+                <Card.Header>Eye Drop</Card.Header>
+                <Card.Body>
+                  <Card.Text>Price: 300TK</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Link to=''>
+                    <button className='btn btn-outline-dark'>BUY NOW</button>
+                  </Link>
+                </Card.Footer>
+              </Card>
+            </Col>
+            <Col>
+              <Card className='card-hover'>
+                <Card.Img variant='top' src={threeShopImg} />
+                <Card.Header>Contact Lenses</Card.Header>
+                <Card.Body>
+                  <Card.Text>Price: 16000TK</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Link to=''>
+                    <button className='btn btn-outline-dark'>BUY NOW</button>
+                  </Link>
+                </Card.Footer>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
--- vision-care-hospital/src/components/Pages/Home/Vision/Vision.js
+++ vision-care-hospital/src/components/Pages/Home/Vision/Vision.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Vision = () => {
   return (
-    <div className='d-flex justify-content-center m-5 p-5'>
+    <div className='d-flex justify-content-center my-3'>
       <Container>
-        <h1>OUR VISION AND MISSION</h1>
-        <p>
-          Vision Care has been established with the slogan “Mission for Vision”.
-          Its journey has been commenced to introduce advanced technologies in
-          eye care field of Bangladesh at an affordable cost and ensure
-          skillness, sincerity and devotion in eye care service. It has been
-          working for prevention of blindness from the society and restoring
-          perfect vision along with the programs of ‘Vision 2020’ run by WHO. It
-          serves purposes of national eye care (NEC) of People’s Republic of
-          Bangladesh in this context.
-        </p>
-        <h3>To reduce the curse of blindness in the society</h3>
-        <p>
-          Make people aware about prevention of blindness and provide
-          specialized treatment for eye patients and rehabilitation of blind and
-          visually handicapped people who can neither be helped by treatment nor
-          surgery.
-        </p>
+        <Row xs={1}>
+          <Col className='d-flex justify-content-center'>
+            <div className=' flex-column d-flex justify-content-center align-items-start my-5 py-5'>
+              <h1>OUR VISION AND MISSION</h1>
+              <p>
+                Vision Care has been established with the slogan “Mission for
+                Vision”. Its journey has been commenced to introduce advanced
+                technologies in eye care field of Bangladesh at an affordable
+                cost and ensure skillness, sincerity and devotion in eye care
+                service. It has been working for prevention of blindness from
+                the society and restoring perfect vision along with the programs
+                of ‘Vision 2020’ run by WHO. It serves purposes of national eye
+                care (NEC) of People’s Republic of Bangladesh in this context.
+              </p>
+              <h3>To reduce the curse of blindness in the society</h3>
+              <p>
+                Make people aware about prevention of blindness and provide
+                specialized treatment for eye patients and rehabilitation of
+                blind and visually handicapped people who can neither be helped
+                by treatment nor surgery.
+              </p>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
